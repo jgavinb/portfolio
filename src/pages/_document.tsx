a1c26f9526fb7edd9c8c27c27729cbfa6e0dd3f4
Gavin <jgbrumfi@ncsu.edu>
2025-02-24 00:03:29 -0500

    Add comment for Google Analytics integration in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Analytics */}
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
