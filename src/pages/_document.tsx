bce4c55e77ec67d949309304fcb248b2848b973b
Gavin <jgbrumfi@ncsu.edu>
2025-02-24 00:06:40 -0500

    Add Google Analytics ID to environment variables and enhance tracking with page path

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,22 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
+        <script
+          async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                page_path: window.location.pathname,
+              });
             `,
           }}
         />
